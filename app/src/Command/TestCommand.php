3abe052b03b82d5857cdb465baf9fc8c2e91c447
Александр <administrator@MacBook-Pro-Aleksandr-2.local>
2024-06-07 15:11:17 +0300

    Очистка тестовой команды

--- app/src/Command/TestCommand.php
+++ app/src/Command/TestCommand.php
@@ -40,32 +40,6 @@ class TestCommand extends Command
 
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
-
-//        $requestId = 'a';
-//
-//        $acceptButton['text'] = '✅ Да';
-//        $acceptButton['callback_data'] = json_encode([
-//            'a_' => $requestId
-//        ], JSON_THROW_ON_ERROR);
-//
-//        $rejectButton['text'] = '❌ Нет';
-//        $rejectButton['callback_data'] = json_encode([
-//            'r_' => $requestId
-//        ], JSON_THROW_ON_ERROR);
-//
-//        $this->botApi->sendMessage(433276358, '', parseMode: 'markdown', replyMarkup: new InlineKeyboardMarkup([
-//            [
-//                $acceptButton, $rejectButton
-//            ]
-//        ]));
-
-//        preg_match('|^/start.*|', ' /start', $matches);
-        preg_match('|^/auth (.*) (.*)$|', '/auth av.vasechkin 12345', $matches);
-
-        dd($matches);
-
-//        $this->sendAllMessagesAction->execute();
-
         return Command::SUCCESS;
     }
 }
